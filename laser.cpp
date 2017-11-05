5884e9c41c5827d20cc4f2f5406c5629a4f3c73b
ohughes343 <owen.hughes271828@gmail.com>
2017-11-05 16:14:26 -0500

    added calibration for light sensor

--- laser.cpp
+++ laser.cpp
@@ -3,18 +3,52 @@ const int button = 8;
 const int led = 10;
 const int sensor = A0;
 
-int threshold = 500;
+int threshold = 700;
 
+
+int sensorMax = 0;
+  int sensorMin = 1023;
+  int sensorValue = 0;
+
+  
 void setup() {
   pinMode(laser, OUTPUT);
   pinMode(button, INPUT);
   pinMode(sensor, INPUT);
   pinMode(led, OUTPUT);
+
+  digitalWrite(led,HIGH);//turn on led1 to signal calibration period
+   
+  //calibrate during first five seconds
+  while(millis() < 5000){
+    sensorValue = analogRead(sensor);
+
+    if(sensorValue > sensorMax){
+        sensorMax = sensorValue;
+      }
+    if(sensorValue < sensorMin){
+        sensorMin = sensorValue;
+      }
+    
+    }
+
+    digitalWrite(led, LOW);//signal end of calibration
+
+    delay(1000);
+  
   
 
 }
 
 void loop() {
+
+   sensorValue = analogRead(sensor);
+  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 255); //apply calibration to sensor         map(value, fromLow, fromHigh, toLow, toHigh)
+  sensorValue = constrain(sensorValue, 0, 255); //in case range is outside 0-255
+
+  int reading = sensorValue; //take data from light sensor
+
+  int threshold = 125;
  
   
   if(digitalRead(button) == HIGH){
@@ -25,7 +59,9 @@ void loop() {
     }
 
 
-  if(analogRead(sensor) > threshold){
+  if(reading > threshold){
     digitalWrite(led, HIGH);
     }
+  else{(digitalWrite(led,LOW));
+  }
 }
